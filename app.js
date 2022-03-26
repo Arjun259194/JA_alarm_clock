20d3117e5e2e1e434fa8b44ca468bf482eb5b9a4
arjun <arjun259194@gmail.com>
2022-03-26 21:31:41 +0530

    created a clock with js will make alarm system next

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 console.log("connected");
+
+let timer = document.querySelector(".timer");
+
+let currantDate = new Date();
+
+let currantTime = () => {
+  let currantDate = new Date();
+
+  let time = {
+    seconds: currantDate.getSeconds(),
+    minutes: currantDate.getMinutes(),
+    hours:
+      currantDate.getHours() > 12
+        ? Math.ceil(currantDate.getHours() % 12)
+        : currantDate.getHours(),
+  };
+
+  timer.innerHTML = `${time.hours}:${time.minutes}:${time.seconds}`;
+
+  setTimeout(() => {
+    currantTime();
+  }, 1000);
+};
+
+currantTime();
